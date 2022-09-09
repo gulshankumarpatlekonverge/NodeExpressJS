bc9e1b293f49db6b649094e181bbb6a876af5a8c
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-09 16:35:43 +0530

    Quick Fix

--- 1-node-tutorial/npm-intro.js
+++ 1-node-tutorial/npm-intro.js
@@ -17,8 +17,13 @@ npm init- step by step, press enter to skip
 npm init -y - everything default
 
 */
+// https://github.com/you-dont-need/You-Dont-Need-Lodash-Underscore
 
-const _lodash = require('lodash');
-const item = [1,[2, [3, [4]]]];
-const newItems = _lodash.flattenDeep(item);
-console.log(newItems);
+
+const _ = require('lodash');
+  
+let ar = [1, [2, [3, 4, [5]]]];
+  
+let flattenArray = _.flattenDeep(ar);
+  
+console.log(flattenArray);
