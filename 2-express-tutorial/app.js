dc0f60db83b96fc92e31d8b593358a9513d61afc
gulshankumar.patlekonverge <gulshankumar.patle@konverge.ai>
2022-09-12 16:33:59 +0530

    finally place your code to execute

--- 2-express-tutorial/app.js
+++ 2-express-tutorial/app.js
@@ -1,47 +1,21 @@
-const { readFileSync } = require('fs');
-const http = require('http');
+const express = require('express');
+const app = express();
 
-const htmlFile = readFileSync('./index.html');
-const homePage = readFileSync('./nav-bar-app/index.html');
-const homePageStyle = readFileSync('./nav-bar-app/style.css');
-const homePageJS = readFileSync('./nav-bar-app/index.js');
-const homePageLogo = readFileSync('./nav-bar-app/logo.png');
-const server = http.createServer((req, res) =>{
-    console.log('User Hit The Server');
-    const url = req.url
-    if(url === '/'){
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write('<h1>Home Page<h1>')
-        res.end('Home Page Details');
-    } else if(url === '/about'){
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write('<h1>About Page<h1>')
-        res.end('About Page Details');
-    } else if(url === '/htmlFile'){
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write(htmlFile)
-        res.end();
-    } else if(url === '/homePage'){
-        res.writeHead(200, {'content-type': 'text/html'});
-        res.write(homePage)
-        res.end();
-    }else if(url === '/style.css'){
-        res.writeHead(200, {'content-type': 'text/css'});
-        res.write(homePageStyle);
-        res.end();
-    }else if(url === '/index.js'){
-        res.writeHead(200, {'content-type': 'text/javascript'});
-        res.write(homePageJS);
-        res.end();
-    }else if(url === '/logo.png'){
-        res.writeHead(200, {'content-type': 'text/png'});
-        res.write(homePageLogo);
-        res.end();
-    } else{
-        res.writeHead(400, {'content-type': 'text/html'});
-        res.write('<h1>Page not found<h1>')
-        res.end(' Page Details not found');
-    }
-});
+/*
 
-server.listen(5000);
+
+    PLACE YOUR CODE HERE FROM "final-code-base" folder to execute 
+
+
+*/
+
+
+// error notation 
+app.all('*', (req, res) => {
+    res.status(400).send('<h1>Page not found</h1>');
+})
+
+// server configuration
+app.listen(5000, () => {
+    console.log("Server is listening on port 5000");
+})
